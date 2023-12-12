4271941917ae54af01321de77351afd5eabcc630
dawidgola <92149687+dawidgola@users.noreply.github.com>
2023-12-12 16:30:48 +0100

    up2

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,19 +1,93 @@
 import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import Drawer from '@mui/material/Drawer';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
+
+
+
+
+
+const rows = [
+  {
+  key:1,
+  players1:['Ewelina','Dawid'],
+  players2: ['Kuba','Krzysztof'],
+  result:'6:2 7:6'
+},
+{
+  key:5,
+  players1:['Ewelina','Dawid'],
+  players2: ['Adam','Andrzej'],
+  result:'6:2 7:6'
+},
+{
+  key:2,
+  players1:['Ewelina','Sławek'],
+  players2:  ['Jerzy','Maksymilian'],
+  result:'1:6 3:6'
+},
+{
+  key:7,
+  players1:['Adam','Andrzej'],
+  players2: ['Ewelina','Sławek'],
+  result:'4:6 6:2 (10:9)'
+},
+{
+  key:3,
+  players1: ['Staszek','Artur'],
+  players2:  ['Ewelina','Sławek'],
+  result:'6:4 6:3'
+},
+{
+  key:6,
+  players1: ['Staszek','Artur'],
+  players2:  ['Adam','Andrzej'],
+  result:'7:5 7:6'
+},
+{
+  key:4,
+  players1: ['Jerzy','Maksymilian'],
+  players2:  ['Kuba','Krzysztof'],
+  result:'6:4 6:2'
+}
+]
 
 
 export default function HomePage() {
   return (
     <Box sx={{ display: 'flex' }}>
+        <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Gracze</TableCell>
+                    <TableCell align="right">Wynik</TableCell>
 
-    Home
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.key}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        <div>
+                          {row.players1.join('-')} / {row.players2.join('-')}
+                        </div>
+
+                      </TableCell>
+                      <TableCell align="right">{row.result}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
     </Box>
   );
 }
