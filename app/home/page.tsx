20b74764e4f4dd897213e15ed6df26f0bc559fd5
David Gola <dawidgola7@gmail.com>
2024-01-21 12:47:53 +0100

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -63,7 +63,7 @@ export default function HomePage() {
    
       <Box sx={{ display: 'flex' }}>
         <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <Table  aria-label="simple table">
                 <TableHead>
                   <TableRow>
                     <TableCell>Gracze</TableCell>
